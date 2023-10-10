03a893ed4ea42487384a57dc7b13e200167404a5
obeda velonjatovo <velonjatovo.obeda@gmail.com>
2023-10-09 17:37:29 -0700

    added icons

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./profile.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
-function Profile() {
+
+function Profile() { 
   return (
     <div className="profile-container flex flex-col gap-y-5 items-center justify-center">
       <img src="./prof_pic.jpeg" alt="profile" className="profile-image" />
       <h2 className="text-2xl">Obeda Velonjatovo</h2>
-      <div className="link">
-        {/* linkin
-        insta
-        git */}
+      <div className="row">
+        <div className="col">
+          <FontAwesomeIcon icon={faLinkedinIn} size="lg"/>
+        </div>
+        <div className="col">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+        </div>
       </div>
     </div>
   );
